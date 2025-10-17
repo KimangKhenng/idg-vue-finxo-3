ac0266e0554982c7e600a7b7efcee6de21168c46
Kim Ang Kheang <kimangkheang@gmail.com>
2025-10-17 11:05:46 +0700

    Components

---
+++ src/components/OurService.vue
@@ -0,0 +1,55 @@
+<template>
+    <section class="service_section layout_padding">
+        <div class="service_container">
+            <div class="container ">
+                <div class="heading_container heading_center">
+                    <h2>
+                        Our <span>Services</span>
+                    </h2>
+                    <p>
+                        There are many variations of passages of Lorem Ipsum available, but the majority have suffered
+                        alteration
+                    </p>
+                </div>
+                <div class="row">
+                    <ServiceBox v-for="(item, index) in services" :title="item.title" :description="item.description"
+                        :icon="item.icon" :key="index" />
+                </div>
+                <div class="btn-box">
+                    <a href="">
+                        View All
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+import ServiceBox from '@/components/ServiceBox.vue';
+export default {
+    components: {
+        ServiceBox
+    },
+    data() {
+        return {
+            services: [
+                {
+                    title: 'Currency Wallet',
+                    description: 'fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using',
+                    icon: '/images/s1.png'
+                },
+                {
+                    title: 'Security Storage',
+                    description: 'fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using',
+                    icon: '/images/s2.png'
+                },
+                {
+                    title: 'Expert Support',
+                    description: 'fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using',
+                    icon: '/images/s3.png'
+                }
+            ]
+        }
+    }
+}
+</script>
